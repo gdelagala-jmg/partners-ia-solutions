135fd1b7bdaba4ce4514bc7a73c3b35a43c6f939
Admin <admin@partners-ia.com>
2026-05-10 21:41:55 +0200

    feat: add #newletter anchor to home page newsletter section

--- src/app/(public)/(main)/HomeClient.tsx
+++ src/app/(public)/(main)/HomeClient.tsx
@@ -219,7 +219,7 @@ export default function HomeClient({ featuredSolutions }: HomeClientProps) {
             <LatestNewsSection />
 
             {/* Newsletter Section */}
-            <section id="newsletter" className="py-10 md:py-20 bg-white scroll-mt-24">
+            <section id="newletter" className="py-10 md:py-20 bg-white scroll-mt-24">
                 <div className="max-w-7xl mx-auto px-5 md:px-6 lg:px-8">
                     <NewsletterForm variant="home" />
                 </div>
